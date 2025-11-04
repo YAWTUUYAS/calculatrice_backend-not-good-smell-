732634ebf8f99044fb6dc274bd7e4ccbc2f8ffa7
Yassine Ben Aba <yassinebenaba17@gmail.com>
2025-11-04 12:14:47 +0100

    version finale refactorise

--- src/main/java/fr/devavance/calculatrice/TypeOperation.java
+++ src/main/java/fr/devavance/calculatrice/TypeOperation.java
@@ -9,5 +9,11 @@ package fr.devavance.calculatrice;
  * @author yassine
  */
 public enum TypeOperation {
+
+    ;
+
+    public static TypeOperation fromCode(String operationNom) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
